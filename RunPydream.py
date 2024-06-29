f6a8eafa2afc422da5a5fac5c2d085a6f0b20d99
ZainRana1101 <zrana@uark.edu>
2024-06-29 14:05:22 -0500

    Updated simulation to run on WithAF2 no_sample parameters and data file

--- RunPydream.py
+++ RunPydream.py
@@ -6,22 +6,11 @@ solver = ScipyOdeSimulator(model)
 sim_protocol = SimulationProtocol(solver)
 
 custom_priors = {'n_Hill': ('uniform', 0.5)}
-no_sample = ['A_init', 'FAD_init', 'Dicarb_init', 'AF2_init', 'AF4_init', 'BCD_init', 'kf_a_binds_af2',
-             'kr_a_binds_af2', 'kf_a_fadnc_binds_af2', 'kr_a_fadnc_binds_af2', 'kf_a_dicarb_binds_af2',
-             'kr_a_dicarb_binds_af2',  'kf_a_af2_binds_fadnc', 'kr_a_af2_binds_fadnc', 'kf_a_af2_binds_dicarb',
-             'kr_a_af2_binds_dicarb', 'kf_a_af2_binds_af4', 'kr_a_af2_binds_af4', 'kf_a_af4_binds_af2',
-             'kr_a_af4_binds_af2', 'kf_a_fadnc_dicarb_binds_af2', 'kr_a_fadnc_dicarb_binds_af2',
-             'kf_a_fadnc_af2_binds_dicarb', 'kr_a_fadnc_af2_binds_dicarb', 'kf_a_fadnc_af2_binds_af4',
-             'kr_a_fadnc_af2_binds_af4', 'kf_a_fadnc_af4_binds_af2', 'kr_a_fadnc_af4_binds_af2',
-             'kf_a_dicarb_af2_binds_fadnc', 'kr_a_dicarb_af2_binds_fadnc', 'kf_a_dicarb_af2_binds_af4',
-             'kr_a_dicarb_af2_binds_af4', 'kf_a_dicarb_af4_binds_af2', 'kr_a_dicarb_af4_binds_af2',
-             'kf_a_af2_af4_binds_fadnc', 'kr_a_af2_af4_binds_fadnc', 'kf_a_af2_af4_binds_dicarb',
-             'kr_a_af2_af4_binds_dicarb', 'k_a_fadnc_dicarb_af2_to_fadc', 'kf_a_fadc_dicarb_af2_binds_af4',
-             'k_a_fadnc_dicarb_af4_binds_af2', 'k_a_fadnc_af2_af4_binds_dicarb', 'k_a_fadc_af2_unbinds_af2',
-             'k_a_fadnc_binds_bcd', 'k_a_fadc_binds_bcd']
+no_sample = ['A_init', 'FAD_init', 'Dicarb_init', 'AF2_init', 'AF4_init', 'BCD_init', 'k_a_fadnc_binds_bcd',
+             'k_a_fadc_binds_bcd']
 
 
-exp_data_file = 'Data/Complex_II_Experiment_Data_NoAF2.csv'
+exp_data_file = 'Data/Complex_II_Experiment_Data_WithAF2.csv'
 
 if __name__ == '__main__':
 
