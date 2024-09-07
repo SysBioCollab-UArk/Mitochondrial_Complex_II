70728c40fcf1a961717b8c3df48f57909e611737
ZainRana1101 <zrana@uark.edu>
2024-09-07 18:35:18 -0500

    Added 2 step simulation experiment to PreEquilTest.py. Next step is to manually adjust parameters to get simulation output more similar to the experimental data. After that, we can try running pydream again.

--- PreEquilTest.py
+++ PreEquilTest.py
@@ -3,19 +3,45 @@ from pysb.simulator import ScipyOdeSimulator
 import numpy as np
 import matplotlib.pyplot as plt
 
-tspan = np.linspace(0, 1, 101)
+model.parameters['AF2_init'].value = 0
 
-sim = ScipyOdeSimulator(model, tspan, verbose=True)
+sim = ScipyOdeSimulator(model, verbose=True)
 
-Result = sim.run()
-print(Result.all.dtype.names)
-# print(model.expressions)
+#pre-equilibration simulation
+tequil = np.linspace(-10, 0, 101)
+Result = sim.run(tequil)
+
+plt.figure("species")
 for i, sp in enumerate(model.species):
     print(i, sp)
-    plt.plot(tspan, Result.all["__s%d" % i], label = "s%d" % i)
+    plt.plot(tequil, Result.all["__s%d" % i], label = "s%d" % i)
 plt.yscale("log")
+plt.ylim(bottom = 1e-5, top = 1e3)
 plt.xlabel("time, min")
 plt.ylabel("concentration, uM")
 plt.legend(loc = 0, ncol = 2, bbox_to_anchor = (1,1))
 plt.tight_layout()
+
+plt.figure("flav")
+plt.plot(tequil, Result.expressions["pct_flavinylation"], lw=2)
+plt.xlabel("time, min")
+plt.ylabel("flavinylation, %")
+plt.tight_layout()
+
+#add dicarboxylate
+
+tspan = np.linspace(tequil[-1], tequil[-1] + 80, 801)
+initials = Result.species[-1]
+initials[2] = 10000
+Result = sim.run(tspan, initials=initials)
+
+plt.figure("species")
+for i, sp in enumerate(model.species):
+    plt.plot(tspan, Result.all["__s%d" % i])
+plt.tight_layout()
+
+plt.figure("flav")
+plt.plot(tspan, Result.expressions["pct_flavinylation"], lw=2)
+plt.tight_layout()
+
 plt.show()
