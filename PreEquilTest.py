6c394e3c71edc45febd442dc3efa7c6780bb376c
lh64 <lh64@cornell.edu>
2024-09-14 19:30:40 -0500

    Updated PreEquilTest.py to plot the experimental data and made a new plot to plot observables related to % flavinylation

--- PreEquilTest.py
+++ PreEquilTest.py
@@ -2,13 +2,19 @@ from complex_II_v3 import model
 from pysb.simulator import ScipyOdeSimulator
 import numpy as np
 import matplotlib.pyplot as plt
+from pysb.util import alias_model_components
+
+alias_model_components()
+
+data = np.genfromtxt("DATA/Complex_II_Experiment_Data_NoAF2.csv", dtype=None, delimiter=",", names=True)
+print(data.dtype.names)
 
 model.parameters['AF2_init'].value = 0
 
 sim = ScipyOdeSimulator(model, verbose=True)
 
 #pre-equilibration simulation
-tequil = np.linspace(-10, 0, 101)
+tequil = np.linspace(-100, 0, 101)
 Result = sim.run(tequil)
 
 plt.figure("species")
@@ -42,6 +48,19 @@ plt.tight_layout()
 
 plt.figure("flav")
 plt.plot(tspan, Result.expressions["pct_flavinylation"], lw=2)
+plt.errorbar(data['time'], data['average'], yerr=data['stderr'], fmt='o', capsize=6)
+plt.tight_layout()
+
+plt.figure('obs')
+plt.plot(tspan, Result.observable(A(af2=None, af4=None, fad=1, dicarb=None) % FAD(a=1, state='nc')), lw=2,
+         label='Obs_A_FADnc')
+plt.plot(tspan, Result.observable(A(af2=None, af4=None, fad=None, dicarb=1) % Dicarb(a=1)), lw=2,
+         label='Obs_A_Dicarb')
+for obs_name in ['Obs_A_FADnc_Dicarb', 'FADc', 'SDHA_tot']:
+    plt.plot(tspan, Result.observables[obs_name], lw=2, label=obs_name)
+plt.xlabel("time, min")
+plt.ylabel("concentration, uM")
+plt.legend(loc=0)
 plt.tight_layout()
 
 plt.show()
