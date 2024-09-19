3fa4a10188c51af14006c1d4bb23bb1c9132b5f7
lh64 <lh64@cornell.edu>
2024-09-19 02:20:43 -0500

    Made some small changes to the three run_complex_II_* files and modified plot_pydream_results.py to create the figures for run_complex_II_w_and_wo_AF2.py, putting both plots on the same figure.

--- run_complex_II_w_and_wo_AF2.py
+++ run_complex_II_w_and_wo_AF2.py
@@ -6,11 +6,13 @@ from copy import deepcopy
 
 model.parameters['AF2_init'].value = 0
 model.parameters['BCD_init'].value = 0
+
 solver_noAF2 = ScipyOdeSimulator(model)
 sim_protocol_noAF2 = SequentialInjections(solver_noAF2, t_equil=100, perturb_time_amount={"Dicarb(a=None)": (0, 10000)})
 
 model2 = deepcopy(model)
 model2.parameters['AF2_init'].value = 10.4  # μM
+
 solver_wAF2 = ScipyOdeSimulator(model2)
 sim_protocol_wAF2 = SequentialInjections(solver_wAF2, t_equil=100, perturb_time_amount={"Dicarb(a=None)": (0, 10000)})
 
@@ -28,7 +30,7 @@ no_sample = ['A_init', 'FAD_init', 'Dicarb_init', 'AF2_init', 'AF4_init', 'BCD_i
              'kr_a_af2_af4_binds_dicarb', 'kf_a_fadc_dicarb_af2_binds_af4', 'k_a_fadnc_dicarb_af4_binds_af2',
              'k_a_fadnc_af2_af4_binds_dicarb', 'k_a_fadc_af4_unbinds_af4', 'k_a_fadnc_binds_bcd', 'k_a_fadc_binds_bcd']
 
-exp_data_file = 'Data/Complex_II_Experiment_Data_With_And_WO_AF2.csv'
+exp_data_file = os.path.join('Data', 'Complex_II_Experiment_Data_With_And_WO_AF2.csv')
 
 if __name__ == '__main__':
 
@@ -38,4 +40,4 @@ if __name__ == '__main__':
                                       priors=custom_priors,
                                       no_sample=no_sample)
 
-    calibrator.run(niterations=50000, nchains=5, plot_results=True)
+    calibrator.run(niterations=50000, nchains=5, plot_results=True, plot_tc_args={'separate_plots': False})
