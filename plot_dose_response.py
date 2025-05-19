4f7b2f1b1af1b11adce6de0441e9a3b955001ecd
lh64 <lh64@cornell.edu>
2025-05-19 01:40:57 -0500

    Small change to plot_dose_response.py

--- plot_dose_response.py
+++ plot_dose_response.py
@@ -5,9 +5,11 @@ basepath = 'SAVED'
 directories = ['Flav_Fumarate', 'Flav_FAD']
 run_pydream_filename = 'run_complex_II_pydream.py'
 expts_doses = [
+    # experiment 0
     {'fum_wAF2': np.array([0, 0.00625, 0.0125, 0.025, 0.05, 0.1, 0.25, 1]) * 1000,
      'fum_noAF2': np.array([0, 0.05, 0.1, 0.25, 0.5, 1]) * 1000,
      'xlabel': r'Fumarate ($\mu$M)'},
+    # experiment 1
     {'fad_wAF2': [0, 1, 2, 4, 6, 20, 40, 100],
      'fad_noAF2': [0, 5, 10, 20, 40, 60, 100],
      'xlabel': r'FAD ($\mu$M)'},
