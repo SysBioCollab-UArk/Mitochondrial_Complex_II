10684253902c8fd30c2dc42e1602f95435efcb9c
lh64 <lh64@cornell.edu>
2025-06-02 06:08:04 -0500

    Updated plot_dose_response.py to include results from 'Flav_Fumarate_FAD'

--- plot_dose_response.py
+++ plot_dose_response.py
@@ -2,7 +2,7 @@ from util import plot_drc
 import numpy as np
 
 basepath = 'SAVED'
-directories = ['Flav_Fumarate', 'Flav_FAD']
+directories = ['Flav_Fumarate', 'Flav_FAD', 'Flav_Fumarate_FAD']
 run_pydream_filename = 'run_complex_II_pydream.py'
 expts_doses = [
     # experiment 0
@@ -13,6 +13,13 @@ expts_doses = [
     {'fad_wAF2': [0, 1, 2, 4, 6, 20, 40, 100],
      'fad_noAF2': [0, 5, 10, 20, 40, 60, 100],
      'xlabel': r'FAD ($\mu$M)'},
+    # experiment 2
+    [{'fum_wAF2': np.array([0, 0.00625, 0.0125, 0.025, 0.05, 0.1, 0.25, 1]) * 1000,
+     'fum_noAF2': np.array([0, 0.05, 0.1, 0.25, 0.5, 1]) * 1000,
+      'xlabel': r'Fumarate ($\mu$M)'},
+     {'fad_wAF2': [0, 1, 2, 4, 6, 20, 40, 100],
+     'fad_noAF2': [0, 5, 10, 20, 40, 60, 100],
+     'xlabel': r'FAD ($\mu$M)'}]
 ]
 label_dict = {'pct_flavinylation': 'Flavinylation', 'fum_wAF2': 'With AF2', 'fum_noAF2': 'W/o AF2',
               'fad_wAF2': 'With AF2', 'fad_noAF2': 'W/o AF2'}
